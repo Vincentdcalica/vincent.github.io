1dcf6fcbdab0b3529d19bb3f61ded4aeac890e12
Vincentdcalica <49674002+Vincentdcalica@users.noreply.github.com>
2019-04-16 12:21:43 +0400

    Add files via upload

---
+++ vincent/project.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Vincent</title>
+  </head>
+  <body>
+
+<!--########################## NAV BAR ###########################-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">VDC</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="features.php">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact</a>
+          </li>
+          <!--<li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Contact
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Facebook</a>
+              <a class="dropdown-item" href="#">Twitter</a>
+              <a class="dropdown-item" href="#">Instagram</a>
+              <a class="dropdown-item" href="#">LinkedIn</a>
+              
+                <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">+971 178 5618</a>
+                </div>
+          </li>-->
+       <!-- <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li> -->
+        </ul>
+        
+      </div>
+    </nav>
+
+    <div class="jumbotron jumbotron-fluid text-center bg-light">
+    <h1 class="display-4">Monitroring of businesses System</h1>
+      <div class="container">
+        <img src="image/bplo.jpg" class="img-fluid img-thumbnail" alt="">
+      </div>
+      <h1 class="display-4">E-Commerse System</h1>
+      <div class="container">
+        <img src="image/lindenteak.jpg" class="img-fluid img-thumbnail" alt="">
+      </div>
+
+      <div class="container">
+        <h1 class="display-4">Scheduling System</h1>
+        <img src="image/scheduling.jpg" class="img-fluid img-thumbnail" alt="">
+      </div>
+    </div>
+
+       
+
+   
+    <!--    <div class="container">
+            <div class="card-deck">
+              <div class="card">
+                <img src="image/lindenteak.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Lindeanteak</h5>
+                  <p class="card-text">2nd project with minimal functions</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Last updated 10 mins ago</small>
+                </div>
+              </div>
+              <div class="card">
+                <img src="image/bplo.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Monitoring System</h5>
+                  <p class="card-text">Thesis project.</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Last updated 20 mins ago</small>
+                </div>
+              </div>
+              <div class="card">
+                <img src="image/scheduling.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Scheduling Sytem</h5>
+                  <p class="card-text">2nd Project (Modified Template)</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Last updated 30 mins ago</small>
+                </div>
+              </div>
+            </div>
+        </div>-->
+
+    <footer class="footer mt-auto py-2">
+        <div class="container text-center">
+            <span class="text-muted">©Copyright VDC</span>
+        </div> 
+    </footer>
+    
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
